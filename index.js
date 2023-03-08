88e02ee7efc881d309e1bf45d6dbecb2ee29f175
Randy D. Binondo <randy.binondo.dev@gmail.com>
2023-03-08 15:15:21 +0000

    Initial commit - working answers with unit tests

---
+++ index.js
@@ -0,0 +1,14 @@
+const CodingAssessment1 = require('./coding-assessment-1');
+
+console.log(CodingAssessment1(35));
+
+const CodingAssessment2 = require('./coding-assessment-2');
+
+console.log(CodingAssessment2(5));
+
+const CodingAssessment3SortingMethod = require('./coding-assessment-3-sorting-method');
+
+const originalValues = [1, 5, 20, 17, 4, 8, 2, 15];
+const result = CodingAssessment3SortingMethod(originalValues);
+
+console.log(result);
